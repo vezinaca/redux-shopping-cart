9da2428e8283a6e17ed2a0fc8fd1ace8ae543618
Carl <vezinaca@gmail.com>
2021-07-04 15:28:40 -0400

    adding responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "./Header.css";
 import imgCart from './cart.png';
+import logoClothes from "../clothes-logo.jpg";
 import { useSelector } from "react-redux";
 import { selectCartInfo } from "../features/cart/cartSlice";
 
  const Header = () => {
 
     const cartInfo = useSelector(selectCartInfo);
+    // const 
+
+
+    const handleActive = () => {
+        console.log('active');
+    }
 
     return (
         <div className="header">
             <nav className="nav">
-                <ul className="ul">
+                
+                <Link to="/store">                        
+                    <li>
+                        <div className="logo">
+                            <i class="fas fa-tshirt fa-lg"></i>
+                            <h1>Clothes</h1>
+                        </div>
+                    </li>                        
+                </Link>
+                <ul className="ul">                    
                     <Link to="/store"><li>Store</li></Link>
                     <Link to="/about"><li>About</li></Link>
                     <Link to="/cart">
+                        <div className="cart-stuff">
                         <li>    
                             <img    className="cartLogo" 
                                     src={imgCart} 
                                     width="15px" 
                                     alt="problem"
                             />
-                                    Cart ({cartInfo.itemCount})
+                            Cart ({cartInfo.itemCount})
                         </li>
+                        </div>
                     </Link>
                 </ul>
+                <div className="hamburger-toggle" onClick={handleActive}>
+                    <i className="fas fa-bars fa-lg"></i>
+                </div>
             </nav>   
         </div>
     )
